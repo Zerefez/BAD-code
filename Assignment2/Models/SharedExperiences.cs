940f6ba5b5e12c37f68fc54e6da62bdbfa5936af
Zerefez <202209849@post.au.dk>
2025-03-19 21:08:15 +0100

    Update Repo

--- Assignment2/Models/SharedExperiences.cs
+++
@@ -1,9 +0,0 @@
-public class SharedExperience
-{
-    public int ExperiencesID { get; set; }
-    public string Name { get; set; }
-    public DateTime Date { get; set; }
-
-    // Relationships
-    public ICollection<Guest> Guests { get; set; } = new List<Guest>();
-}
